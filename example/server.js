edd9fe1ba556ceaedc4e76960c1603723fe94866
Nir Adler <niradler55@gmail.com>
2019-01-08 22:47:26 +0200

    remove example moudles

--- example/server.js
+++ example/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const NextMiddleware = require('nextjs-middleware');
+const NextMiddleware = require('../index');
 const main = async() => {
     try {
         const debug = true;
@@ -9,21 +9,21 @@ const main = async() => {
         server.get('/', async(req, res) => {
             return res
                 .view
-                .handle({})
+                .handle({});
         })
         server.get('/example', async(req, res) => {
             if (!req.parsedUrl.query.page) {
-                return res.redirect('/example?page=2')
+                return res.redirect('/example?page=2');
             }
             return res
                 .view
-                .render({req,res,path:'/examplePage',query:req.parsedUrl.query})
+                .render({req,res,path:'/examplePage',query:req.parsedUrl.query});
         })
 
         server.listen(3000, (err) => {
             if (err) 
                 throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log('> Ready on http://localhost:3000');
         })
     } catch (error) {
         console.log({error})
